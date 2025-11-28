a33d2a31db7295f15aa191cb8934b2c786958a67
itznasiba <zeynallinesibe2006@gmail.com>
2025-11-28 13:17:30 +0000

    1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main-prints a message 
+ * main-prints a message
  *
  * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n"):
 	return (0);
 }
-
