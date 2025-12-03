8fcb47ae3e5932526b8031a19b29ff4c1bfe1961
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-03 14:09:03 +0000

    1

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,13 @@
  * return: Always 0 (Success)
  */
 
-int main(void){
+int main(void)
+{
 	char c ='a';
 
-	while( c<= 'z')
-		putchar(c++);
-	putchar('\n');
+	while (c <= 'z')
+		putchar (c++);
+	putchar ('\n');
 
 	return (0);
 }
