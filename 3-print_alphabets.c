1e6b4fe6e0c2f3c4561344e5002ec78db907694c
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-03 19:35:35 +0000

    1

---
+++ 3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase and then uppercase
+ *
+ * Return: 0 in success
+ */
+
+int main(void)
+{
+	char c ='a';
+
+	while c <= 'z'
+		putchar(c);
+		c++;
+	putchar('\n');
+	return (0);
+}
+		
