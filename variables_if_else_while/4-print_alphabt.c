1d0973b145aa1d23b549f4ca8cee6737df1c0c5f
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-04 12:25:15 +0000

    1

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase and then uppercase
+ * main - prints the alphabet in lowercase, except e and q
  *
  * Return: 0 in success
  */
 
 int main(void)
 {
-        char c = 'a';
-
-        while (c <= 'z')
-        {
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
 		if (c != 'q' && c != 'e')
 			putchar(c);
-        	c++;
-        }
-
-        putchar('\n');
-        return (0);
+		c++;
+	}
+	
+	putchar('\n');
+	return (0);
 }
