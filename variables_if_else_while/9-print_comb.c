8e63f7be4b117091d1de51d8f6a32355deaee787
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-06 20:46:55 +0000

    1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,16 @@ int main(void)
 {
 	int n = '0';
 
-	while (n < '9')
+	while (n <= '9')
         {
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
