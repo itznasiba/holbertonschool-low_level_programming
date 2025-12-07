6490d624d066f657609ef967f2d1c4c7fb9f0e73
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-07 06:59:38 +0000

    1

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,16 @@
 
 void print_alphabet_x10(void)
 {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i, j;
-
-    for (i = 0; i < 10; i++)
-    {
-        j = 0;
-        while (alphabet[j] != '\0')
-        {
-            _putchar(alphabet[j]);
-            j++;
-        }
-    }
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		j = 0;
+		while (alphabet[j] != '\0')
+		{
+			_putchar(alphabet[j]);
+			j++;
+		}
+	}
 }
-
