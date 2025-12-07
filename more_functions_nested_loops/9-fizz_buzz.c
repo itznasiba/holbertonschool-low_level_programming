d37865db68552e6b99a74ac74b26667367b162f6
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-07 14:06:09 +0000

    1

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - prints numbers from 1 to 100, followed by a new line
+ * main - prints all the numbers from 1 to 100, but for multipliers of 3 Fizz, multipliers of 5 Buzz, multipliers of both 3 and 5 FizzBuzz
  *
  * Return: 0 in success
  */
 
 int main(void)
 {
-	int a = 1;
+	int a;
+
+	a = 0;
 
 	while (a <= 100)
 	{
 		if (a % 3 == 0 && a % 5 != 0)
-			printf("Fizz ");
-		if (a % 5 ==0 && a % 3 !=0)
-			printf("Buzz ");
+			printf("Fizz");
+		if (a % 5 == 0 && a % 3 != 0)
+			printf ("Buzz");
 		if (a % 5 == 0 && a % 3 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		if (a % 5 != 0 && a % 3 != 0)
-			printf("%d ",a);
+			printf("%d",a);
+		if (a != 100)
+			printf(" ");
 		a++;
 	}
-
 	return (0);
 }
