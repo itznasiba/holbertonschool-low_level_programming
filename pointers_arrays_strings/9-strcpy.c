c676b5e8a5d58db890e7acb9cf8b38c1afac068e
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-09 12:28:41 +0000

    1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - copy the string pointed by src
+ * @src: string to be pointed
+ * @dest: string to write copied src
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
