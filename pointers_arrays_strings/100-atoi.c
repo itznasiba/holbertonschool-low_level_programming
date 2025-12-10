3683597113ed4b0a17239f9a9c7f86f71704ee90
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-10 18:39:52 +0000

    1

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - turns a string to an integer with its sign
+ * @s: a st4ring to be turned into an integer
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			sign = sign * (-1);
+		if (*s <= '0' && *s >= '9')
+			break
+	}
+
+	while (*s != '\0')
+	{
+		if (*s <= '9' && *s >= '0'){
+		
