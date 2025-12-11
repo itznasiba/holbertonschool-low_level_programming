e52d17c2bf195fae4252a96c0ab0c738ef9a37f7
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-11 14:15:58 +0000

    1

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i+j] = src[j];
+	}
+
+	dest[i+j] = '\0';
+
+	return (dest);
+}
