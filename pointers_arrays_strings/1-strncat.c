486e44f45a3e49583ec213f32488cc26e253119f
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-11 20:18:15 +0000

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,8 +3,9 @@
 
 /**
  * _strncat - concatenates two strings
- * @dest: the first string 
+ * @dest: the first string
  * @src: the second string
+ * @n: the number of elements to be appended from src
  *
  * Return: char
  */
@@ -28,5 +29,4 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i + j] = '\0';
 
 	return (dest);
-
 }
