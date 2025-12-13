8e5b266de305e2b03b874687c817cda420ab8dd3
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-13 13:00:21 +0000

    1

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,16 +11,23 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+
 	if (str[0] >= 'a' && str[0] <= 'z')
 	{
-		str[0] = str[0] -32;
+		str[0] = str[0] - 32;
 	}
 
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <='z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == '\n' || str[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"')
+			if (str[i - 1] == ' ' || str[i - 1] == '.' ||
+				str[i - 1] == '\n' || str[i - 1] == '\t' ||
+				str[i - 1] == ',' || str[i - 1] == ';' ||
+				str[i - 1] == '(' || str[i - 1] == ')' ||
+				str[i - 1] == '{' || str[i - 1] == '}' ||
+				str[i - 1] == '!' || str[i - 1] == '?' ||
+				str[i - 1] == '"')
 			{
 				str[i] = str[i] - 32;
 			}
@@ -28,4 +35,4 @@ char *cap_string(char *str)
 		i++;
 	}
 	return (str);
-}			
+}
