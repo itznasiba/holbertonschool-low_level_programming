24838c601a6888c9b16edacd4edd181a30e9e077
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-13 12:22:48 +0000

    1

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ char *cap_string(char *str)
 	{
 		if (str[i] >= 'a' && str[i] <='z')
 		{
-			if (str[i-1] == ' ' || str[i-1] == '.' || str[i-1] == '\n' || str[i-1] == '\t' || str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{' || str[i-1] == '}' || str[i-1] == '!' || str[i-1] == '?' || str[i-1] == '"')
+			if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == '\n' || str[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"')
 			{
 				str[i] = str[i] - 32;
 			}
