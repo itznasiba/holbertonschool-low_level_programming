570bb88e41c695b2e0e8bee45fd27e5fb308aa5f
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-14 20:14:11 +0000

    1

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: an array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c",a[i][j]);
+		}
+		printf("\n");
+	}
+}
