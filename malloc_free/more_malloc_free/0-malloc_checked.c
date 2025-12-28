00380b12007a002b20bfcd8b3825291f8442899b
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-28 19:11:03 +0000

    1

---
+++ malloc_free/more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: an unsigned int
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ malloc_free/more_malloc_free/README.md
@@ -0,0 +1,1 @@
+readme fille of project
