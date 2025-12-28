8ea3282aaa58f5eb467c2be50d1cb3cad4780b5d
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-28 20:05:47 +0000

    1

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,31 +9,25 @@
  *
  * Return: pointer to the newly created array, or NULL if it fails
  */
+
 int *array_range(int min, int max)
 {
-    int *array;
-    int i, size;
+	int *array;
+	int i, size;
 
-    // 1. If min > max, return NULL
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    // 2. Calculate the number of elements (inclusive)
-    size = (max - min) + 1;
+	size = (max - min) + 1;
+	array = malloc(sizeof(int) * size);
+	if (array == NULL)
+		return (NULL);
 
-    // 3. Allocate memory for 'size' number of integers
-    array = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = min;
+		min++;
+	}
 
-    // 4. Check if malloc failed
-    if (array == NULL)
-        return (NULL);
-
-    // 5. Fill the array with values from min to max
-    for (i = 0; i < size; i++)
-    {
-        array[i] = min;
-        min++;
-    }
-
-    return (array);
+	return (array);
 }
