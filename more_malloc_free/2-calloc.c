aae42c2c0247c269e5b6ebb8137d94234d5f15ae
itznasiba <zeynallinesibe2006@gmail.com>
2025-12-28 19:59:54 +0000

    1

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int *arr;
+	unsigned int i;
+
+	arr = _calloc(5, sizeof(int));
+	if (arr == NULL)
+		return (1);
+
+	for (i = 0; i < 5; i++)
+		printf("%d ", arr[i]); // Output: 0 0 0 0 0
+	printf("\n");
+
+	free(arr);
+	return (0);
+}
