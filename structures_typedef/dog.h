1585c5d6cda2a6459714425146001f0e4dff2b79
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-01 10:53:09 +0000

    1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * struct dog - structure for a dog
+ * @name: the name of a dog
+ * @age: the age of a dog
+ * @owner: the name of the owner
+ */
+
 struct dog
 {
 	char *name;
