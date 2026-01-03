f6583d3c96188b0cc811d273f55a9458eb2d65ee
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-03 14:01:47 +0000

    1

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,10 +19,17 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 
 	op_funct = get_op_func(argv[2]);
+	
+	if (argv[2][1] != '\0')
+	{
+		printf ("Error\n");
+		return (99);
+	}
+
 	if (op_funct == NULL)
 	{
 		printf("Error\n");
-		return (98);
+		return (99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
