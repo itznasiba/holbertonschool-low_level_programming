ad3ba430c7f4569675ec54077c3c375e234a5972
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-03 11:11:31 +0000

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,9 +13,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	size_t i = 0;
 
-	if (array == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	while (i < size)
