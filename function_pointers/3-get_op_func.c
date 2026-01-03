c8caacea4098dfe9e88cd5ed60e7f51d3301a726
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-03 13:06:42 +0000

    1

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - chooses which function to call
+ * @s: input 
+ *
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op,s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return NULL;
+}
