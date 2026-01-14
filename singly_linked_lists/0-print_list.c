1a323506dd481867b7953aaceff0bb129f3591f6
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-14 13:36:33 +0000

    1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,13 +14,13 @@ size_t print_list(const list_t *h)
 
 	count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n",h->len,h->str);
-		h=h->next;
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		count++;
 	}
 	return (count);
