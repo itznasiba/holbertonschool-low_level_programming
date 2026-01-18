cbcfe3c91cbfca07a8eb8d3ef2bf54df124b10c1
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-18 13:56:32 +0400

    free a list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -13,10 +13,9 @@
 void free_dlistint(dlistint_t *head)
 {
         dlistint_t *temp;
-        while (head != NULL)
-        {
-            temp = head->next;
-            free(head);
-            head = temp;
-        }
+
+        while (head != NULL){
+            temp = head;
+            head = head->next;
+            free(temp);}
 }
