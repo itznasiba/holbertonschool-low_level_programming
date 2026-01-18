4b8a8630bc5727b141f8fade338a3531cd969476
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-18 15:08:35 +0400

    sum list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -12,18 +12,18 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    if (head == NULL)
-    {
-        return (0);
-    }
+	if (head == NULL)
+	{
+		return (0);
+	}
 
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-    return (sum);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
 
