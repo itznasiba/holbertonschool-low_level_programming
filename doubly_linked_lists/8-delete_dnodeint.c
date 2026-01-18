2afc185822ba4d3bc43697b34d13966d912d8d92
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-18 20:26:06 +0400

    8-delete_nodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,18 +11,14 @@
  * Return: int
  */
 
- int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *temp;
 
 	if (head == NULL || *head == NULL)
-	{
 		return (-1);
-	}
-
 	temp = *head;
-
 	if (index == 0)
 	{
 		*head = temp->next;
@@ -33,30 +29,17 @@
 		free(temp);
 		return (1);
 	}
-
 	while (temp != NULL && i < index)
 	{
 		temp = temp->next;
 		i++;
 	}
-
 	if (temp == NULL)
-	{
 		return (-1);
-	}
-
-
 	if (temp->prev != NULL)
-	{
 		temp->prev->next = temp->next;
-	}
-
 	if (temp->next != NULL)
-	{
 		temp->next->prev = temp->prev;
-	}
-
 	free(temp);
 	return (1);
-
 }
