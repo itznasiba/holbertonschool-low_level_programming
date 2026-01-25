7a61f360b1236819c00f0271869022030932b9f1
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-25 23:29:35 +0400

    0 file io

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
  * @letters: number of letters to read and print
- * 
+ *
  * Return: actual number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
