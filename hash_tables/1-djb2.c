ad7fcf4ba776e91d61e4aafbf752acc4af7c331f
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-29 13:40:54 +0000

    1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - implements djb2
+ * @str: string
+ *
+ * Return: unsigned long int
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
