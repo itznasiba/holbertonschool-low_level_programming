f3a80a7930fa50a2d6aadebc066d08b3e1b8023c
itznasiba <zeynallinesibe2006@gmail.com>
2026-02-24 13:03:34 +0000

    1

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -15,15 +15,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (ht == NULL || key == NULL || key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	index = key_index((unsigned int char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
 	while (node != NULL)
 	{
-		if (strcmp(node_key, key) == 0)
+		if (strcmp(node->key, key) == 0)
 			return (node->value);
 		node = node->next;
 	}
