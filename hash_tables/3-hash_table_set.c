09e30fdefe1c13b69c909b3454750c3d22662079
itznasiba <zeynallinesibe2006@gmail.com>
2026-02-24 12:29:35 +0000

    1

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,6 +18,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	char *new_value;
 
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
 	new_value = strdup(value);
 	if (value == NULL)
 		return (0);
